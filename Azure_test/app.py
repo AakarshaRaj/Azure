fc4efabeb467e45327b1f79caecf86b14f69b444
AakarshaRaj <125351949+AakarshaRaj@users.noreply.github.com>
2025-06-20 02:08:40 +0530

    Update app.py

--- Azure_test/app.py
+++ Azure_test/app.py
@@ -53,4 +53,5 @@ def submit():
         return f"An error occurred: {str(e)}", 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    app.run(host='0.0.0.0', port=8000)
